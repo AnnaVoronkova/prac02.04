5827e60476fd69c14401606a5f462c37bd220206
AnnaVoronkova <63228705+AnnaVoronkova@users.noreply.github.com>
2021-04-02 11:20:16 +0300

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,39 +1,18 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 int main(){
-    int a, b;
-    ifstream input ("1.txt");
-    input >> a >> b;
-    int mas[b];
-    for (int i = 0; i < b; i++){
-        input >> mas[i];
-    }
-    int t = 0;
-    for (int i = 0; i < b; i++){
-        for(int j = 0; j < b - 1; j++){
-            if (mas[j] > mas[j+1]){
-                t = mas[j];
-                mas[j] = mas[j+1];
-                mas[j+1] = t;
+    int k = 0;
+    for(int i = 2422000; i <= 2422080; i++){
+        for(int j = 2; j < i; j++){
+            if(i%j == 0){
+               k++;
             }
         }
-    }
-    int m = 0;
-    for(int i = 0; i < b; i++){
-        if (a - mas[i] >= 0 ){
-            a -= mas[i];
-            m++;
+        if (k == 0){
+            cout << i << " ";
         }
+        k = 0;
     }
-    cout << m << " ";
-    for(int i = m; i < b; i++){
-        if(a + mas[m-1] - mas[i] >= 0){
-            a = a + mas[m-1] - mas[i];
-            m = i + 1;
-        }
-    }
-    cout << mas[m - 1];
     return 0;
 }
